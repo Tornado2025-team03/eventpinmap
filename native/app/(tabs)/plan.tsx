4116bfec950ef1d53dbd67f4fe1808a7a1434a4a
Koyu Fuke <kouyuu6@gmail.com>
2025-08-15 23:20:42 +0900

    feat: added tabs

---
+++ native/app/(tabs)/plan.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PlanScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>見る</Text>
+    </View>
+  );
+}
